4f1a223970b74def0f1f6effdcda90f1c7b1b7c5
Jibpreeya <107095163+Jibpreeya@users.noreply.github.com>
2022-08-02 16:51:38 +0700

    mergeของจิ๊บ

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Post } from './Pages/Post/Post';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import { useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
@@ -12,13 +12,13 @@ function App() {
   return (
     <div className='app'>
       <Post />
-      {/* <BrowserRouter>
-        <Navbar />
+      <BrowserRouter>
+        {/* <Navbar /> */}
         <Routes>
           <Route path='/' element= {<Login />} />
           <Route path='/Register' element= {<Register />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   )
     
