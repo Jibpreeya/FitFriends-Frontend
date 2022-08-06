1cc412c19746b6b9e54a9b8fdf3343d983b40801
Jiroge <s.jiroge@gmail.com>
2022-08-06 13:20:45 +0700

    First combination from Joe

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { Post } from './Pages/Post/Post';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import './App.scss';
+import Navbar from './Components/Navbar/Navbar';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
+import MainPage from './Pages/MainPage/MianPage';
+import { Post } from './Pages/Post/Post';
+import ProfilePage from './Pages/ProfilePage/ProfilePage'
 
 
 function App() {
   return (
     <div className='app'>
-      <Post />
+      {/* พื้นที่สำหรับรัน test แต่ละ page */}
+        {/* <Login /> */}
+        {/* <Register /> */}
+        {/* <MainPage /> */}
+        {/* <Post /> */}
+        {/* <ProfilePage /> */}
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />
         <Routes>
           <Route path='/' element= {<Login />} />
           <Route path='/Register' element= {<Register />} />
+          <Route path='/MainPage' element= {<MainPage />} />
+          <Route path='/Post' element= {<Post />} />
+          <Route path='/ProfilePage' element= {<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
