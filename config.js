f38db32933e237a813809d02b4b47e6d6ceb5fd5
Jiroge <s.jiroge@gmail.com>
2022-08-24 13:13:00 +0700

    update config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
-// const config = {
-//   url:"https://backend-test-vercel-offtion96.vercel.app"
-//     }
-// export default config
-
 const config = {
-  url:"http://localhost:8080"
+  url:"https://backend-test-vercel-offtion96.vercel.app"
     }
-
 export default config
+
+// const config = {
+//   url:"http://localhost:8080"
+//     }
+
+// export default config
